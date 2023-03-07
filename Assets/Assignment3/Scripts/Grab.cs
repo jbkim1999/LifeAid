e79b17c4bb36b322ccd47dde88725bf56a5dc99e
nishantrai-nus <e0560182@u.nus.edu>
2023-03-07 16:46:08 +0800

    add firing and targets

--- Assets/Assignment3/Scripts/Grab.cs
+++ Assets/Assignment3/Scripts/Grab.cs
@@ -4,67 +4,69 @@ using UnityEngine;
 
 public class Grab : MonoBehaviour
 {
-    public OVRInput.Controller Controller;
-    public string buttonName;
-    public float grabRadius;
-    public LayerMask grabMask;
+  public OVRInput.Controller Controller;
+  public string buttonName;
+  public float grabRadius;
+  public LayerMask grabMask;
+  public GameObject objectToSpawn;
+  public float shootMultiplier = 1000f;
 
-    private GameObject grabbedObject;
-    private bool grabbing;
-    // Start is called before the first frame update
-    void Start()
+  private GameObject grabbedObject;
+  private bool grabbing;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    if (!grabbing && Input.GetAxis(buttonName) == 1)
     {
-
+      GrabObject();
     }
-
-    // Update is called once per frame
-    void Update()
+    if (grabbing && Input.GetAxis(buttonName) < 1)
     {
-        if (!grabbing && Input.GetAxis(buttonName) == 1)
-        {
-            GrabObject();
-        }
-        if (grabbing && Input.GetAxis(buttonName) < 1)
-        {
-            DropObject();
-        }
+      DropObject();
     }
+  }
 
-    void GrabObject()
+  void GrabObject()
+  {
+    grabbing = true;
+    RaycastHit[] hits;
+    hits = Physics.SphereCastAll(transform.position, grabRadius, transform.forward, 0f, grabMask);
+    if (hits.Length > 0)
     {
-        grabbing = true;
-        RaycastHit[] hits;
-        hits = Physics.SphereCastAll(transform.position, grabRadius, transform.forward, 0f, grabMask);
-        if (hits.Length > 0)
+      int closestHit = 0;
+      for (int i = 0; i < hits.Length; i++)
+      {
+        if ((hits[i]).distance < hits[closestHit].distance)
         {
-            int closestHit = 0;
-            for (int i = 0; i < hits.Length; i++)
-            {
-                if ((hits[i]).distance < hits[closestHit].distance)
-                {
-                    closestHit = i;
-                }
-            }
-
-            grabbedObject = hits[closestHit].transform.gameObject;
-            grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
-            grabbedObject.transform.position = transform.position;
-            grabbedObject.transform.parent = transform;
+          closestHit = i;
         }
-    }
+      }
 
-    void DropObject()
+      grabbedObject = hits[closestHit].transform.gameObject;
+      grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
+      grabbedObject.transform.position = transform.position;
+      grabbedObject.transform.parent = transform;
+    }
+  }
+
+  void DropObject()
+  {
+    grabbing = false;
+    if (grabbedObject != null)
     {
-        grabbing = false;
-        if (grabbedObject != null)
-        {
-            grabbedObject.transform.parent = null;
-            grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+      grabbedObject.transform.parent = null;
+      grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
 
-            grabbedObject.GetComponent<Rigidbody>().velocity = OVRInput.GetLocalControllerVelocity(Controller);
-            grabbedObject.GetComponent<Rigidbody>().angularVelocity = OVRInput.GetLocalControllerAngularVelocity(Controller);
-
-            grabbedObject = null;
-        }
+      // fire object
+      grabbedObject.GetComponent<Rigidbody>().AddForce(OVRInput.GetLocalControllerRotation(Controller) * Vector3.forward * shootMultiplier);
+      grabbedObject = null;
+      Instantiate(objectToSpawn, new Vector3(0.1f, 1f, 0.5f), Quaternion.identity);
     }
+  }
 }
