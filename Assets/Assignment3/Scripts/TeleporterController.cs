5c95ebae1f63902da2e6e785136cf3a82c87ec2e
saishachhabria <saishachhabria02@gmail.com>
2023-03-10 15:46:05 +0800

    Added teleportation

---
+++ Assets/Assignment3/Scripts/TeleporterController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterController : MonoBehaviour {
+
+    public Teleporter teleporter;
+    public string buttonName;
+
+	void Update () {
+
+        if (Input.GetAxis(buttonName) == 1) // Change to VR Controller
+        {
+            teleporter.ToggleDisplay(true);
+        }
+
+        if(Input.GetAxis(buttonName) < 1) // Change to VR Controller
+        {
+            teleporter.Teleport();
+            teleporter.ToggleDisplay(false);
+        }
+	}
+}
